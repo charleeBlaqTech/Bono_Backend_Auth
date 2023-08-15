9a87743bb2aedf8ad4d20108a31c253a112062a8
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2023-08-15 21:52:57 +0100

    update on google  controller req.user

--- controllers/googleAuthController.js
+++ controllers/googleAuthController.js
@@ -19,22 +19,27 @@ const googleLoginCallBack= async (req, res)=>{
     
     const secret = config.get('secret_token');
     try {
-        if(req.user){
-            const currentUserDetail= req.user;
-            const verifiedUser= CheckUser(req.user);
-            if(verifiedUser){
-                const verifiedUserId= verifiedUser._id;
-                const accessToken= await jwt.sign(({verifiedUserId}), secret,{expiresIn: 420});         
-                res.cookie('auth',accessToken,{maxAge:420000, httpOnly: true, sameSite: "lax"});
-                res.status(200).render('profile',{currentUserDetail, loggedIn});
-            }else{
-                const verifiedUserId= req.user._id;
-                const accessToken= await jwt.sign(({verifiedUserId}), secret,{expiresIn: 420});         
-                res.cookie('auth',accessToken,{maxAge:420000, httpOnly: true, sameSite: "lax"});
-                res.redirect('/customer/signup/complete');
-            }
+        // if(req.user){
+        //     const currentUserDetail= req.user;
+        //     const verifiedUser= CheckUser(req.user);
+        //     if(verifiedUser){
+        //         const verifiedUserId= verifiedUser._id;
+        //         const accessToken= await jwt.sign(({verifiedUserId}), secret,{expiresIn: 420});         
+        //         res.cookie('auth',accessToken,{maxAge:420000, httpOnly: true, sameSite: "lax"});
+        //         res.status(200).render('profile',{currentUserDetail, loggedIn});
+        //     }else{
+        //         const verifiedUserId= req.user._id;
+        //         const accessToken= await jwt.sign(({verifiedUserId}), secret,{expiresIn: 420});         
+        //         res.cookie('auth',accessToken,{maxAge:420000, httpOnly: true, sameSite: "lax"});
+        //         res.redirect('/customer/signup/complete');
+        //     }
             
-        }
+        // }
+        const currentUserDetail= req.user
+        const verifiedUserId= req.user._id;
+        const accessToken= await jwt.sign(({verifiedUserId}), secret,{expiresIn: 420});         
+        res.cookie('auth',accessToken,{maxAge:420000, httpOnly: true, sameSite: "lax"});
+        res.status(200).render('profile',{currentUserDetail, loggedIn});
     } catch (error) {
         console.log(error)
         res.redirect('/customer/login');
