bb9f1dd8b45b95d30746b68c3957597657f97fd1
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2023-08-16 11:47:18 +0100

    title fixed and some additional styles added

--- public/js/script.js
+++ public/js/script.js
@@ -3,17 +3,22 @@ const hamburgerMenu= document.querySelector('.hamburger');
 const exitMenu= document.querySelector('.menu-exit-wrapper');
 const ulList= document.querySelector('.nav-menu');
 const showPassword= document.querySelector('#show-password');
-const newPasswordInputs= document.querySelectorAll('.newPassword');
+const newPasswordInputs= document.querySelectorAll('.showPassword');
 
 
+
+
+//FOR HAMBURGER TO SHOW AND HIDE NAV MENU WHEN ON SMALLER DEVICE SCREEN
 hamburgerMenu.addEventListener('click', ()=>{
     ulList.classList.toggle('showNavMenu');
 })
 
+//FOR NAV MENU EXIT TO HIDE NAV MENU
 exitMenu.addEventListener('click', ()=>{
-    ulList.classList.toggle('showNavMenu');
+    ulList.classList.remove('showNavMenu');
 })
 
+// TO SHOW SHOW AND HIDE PASSWORDS
 showPassword.addEventListener('click', (e)=>{
     e.preventDefault();
     newPasswordInputs.forEach(input=>{
